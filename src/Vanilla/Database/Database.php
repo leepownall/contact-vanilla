10000ad0dca75d37bd1a064a568ef1f5dff87ce8
Lee Pownall <leepownall@gmail.com>
2013-10-31 16:21:20 +0000

    Database.php now only handles setup

--- src/Vanilla/Database/Database.php
+++ src/Vanilla/Database/Database.php
@@ -1,6 +1,6 @@
 <?php namespace Vanilla\Database;
 
-class Database
+abstract class Database
 {
     protected $db;
 
@@ -10,30 +10,6 @@ class Database
             $this->db = new \PDO('mysql:host=localhost;dbname=contact-vanilla;', 'root', '');
             $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             $this->db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
-        } catch(\PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function all()
-    {
-        try {
-            $smt = $this->db->prepare('SELECT * FROM messages');
-            $smt->execute();
-            return $smt->fetchAll();
-        } catch(\PDOException $e) {
-            echo $e->getMessage();
-        }
-    }
-
-    public function insert($name, $email, $message)
-    {
-        try {
-            $smt = $this->db->prepare('INSERT INTO messages (name, email, message) VALUES (:name, :email, :message)');
-            $smt->bindParam(':name', $name);
-            $smt->bindParam(':email', $email);
-            $smt->bindParam(':message', $message);
-            $smt->execute();
         } catch(\PDOException $e) {
             echo $e->getMessage();
         }
