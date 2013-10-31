5c4b13e10e5cf51da1c12683872b7380d38dd5d5
Lee Pownall <leepownall@gmail.com>
2013-10-31 16:20:28 +0000

    Updated styling and added navigation

--- messages.php
+++ messages.php
@@ -15,17 +15,43 @@
     </head>
     <body>
         <div class="container">
+            <nav class="navigation">
+                <ul>
+                    <li><a href="index.php">Add Message</a></li>
+                    <li><a href="messages.php" class="selected">All Messages</a></li>
+                </ul>
+            </nav>
+            <?php              
+                $messages = new Vanilla\Database\Message;
+                if(count($messages->all()) > 0) {
+                    foreach($messages->all() as $message) {
+            ?>
+                        <div class="message">
+                            <div class="message-row">
+                                <span class="message-label">Name:</span>
+                                <span><?php echo $message->name; ?></span>
+                            </div>
+                            <div class="message-row">
+                                <span class="message-label">Email:</span>
+                                <span><?php echo $message->email; ?></span>
+                            </div>
+                            <div class="message-row">
+                                <span class="message-label">Date Sent:</span>
+                                <span><?php echo $message->created_at; ?></span>
+                            </div>
+                            <div class="message-row">
+                                <span class="message-label">Message:</span>
+                                <span class="actual-message">
+                                    <?php echo $message->message; ?>
+                                </span>
+                            </div>
+                        </div>
             <?php
-                $db = new Vanilla\Database\Database;
-
-                foreach($db->all() as $message) { ?>
-                    <div class="message">
-                        <header>
-                            <?php echo $message->name . " - " . $message->email; ?>
-                        </header>
-                        <p><?php echo $message->message; ?></p>
-                    </div>
-                <?php } ?>
+                    }
+                } else {
+                    echo "<span class='no-messages'>No messages to display.</span>";
+                }
+            ?>
         </div>
 
         <!-- Type Kit -->
